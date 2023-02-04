1119ff0ff988f963870f133be30fdfc1b4b20a2f
uncorner <denis.uncorner@gmail.com>
2023-02-04 20:00:35 +0300

    Contact props as nullable

--- SampleWebSite/SampleWebSite/Models/Contact.cs
+++ SampleWebSite/SampleWebSite/Models/Contact.cs
@@ -6,24 +6,24 @@ namespace SampleWebSite.Models
     {
         [Display(Name = "Имя")]
         [Required(ErrorMessage = "Введите имя")]
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
         [Display(Name = "Фамилия")]
         [Required(ErrorMessage = "Введите фамилию")]
-        public string Surname { get; set; }
+        public string? Surname { get; set; }
 
         [Display(Name = "Возраст")]
         [Required(ErrorMessage = "Введите возраст")]
-        public int Age { get; set; }
+        public int? Age { get; set; }
 
         [Display(Name = "Почта")]
         [Required(ErrorMessage = "Введите почту")]
-        public string Email { get; set;}
+        public string? Email { get; set;}
 
         [Display(Name = "Сообщение")]
         [Required(ErrorMessage = "Введите сообщение")]
         [StringLength(30, ErrorMessage = "Сообщение не должно содержать более 30 символов")]
-        public string Message { get; set;}
+        public string? Message { get; set;}
 
         
     }

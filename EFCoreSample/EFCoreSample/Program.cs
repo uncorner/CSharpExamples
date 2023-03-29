9031cefcf88660588a50c41fc2bcde2394d3fdcb
uncorner <denis.uncorner@gmail.com>
2023-03-29 19:39:41 +0300

    add Database.EnsureDeleted

--- EFCoreSample/EFCoreSample/Program.cs
+++ EFCoreSample/EFCoreSample/Program.cs
@@ -1,7 +1,6 @@
 ﻿using EFCoreSample.src;
 using Microsoft.Extensions.Configuration;
 
-
 internal class Program
 {
     private static IConfiguration configuration = null!;
@@ -9,30 +8,41 @@ internal class Program
     private static void Main(string[] args)
     {
         configuration = GetAppSettingsFile();
-        var connString = configuration.GetConnectionString("Default");
-        if (connString is null)
+        
+        using (var dbContext = CreateDBContext())
         {
-            throw new NullReferenceException("Default connection string is empty");
+            dbContext.Database.EnsureDeleted();
         }
 
-        using (ApplicationContext db = new(connString))
+        using (var dbContext = CreateDBContext())
         {
             var user1 = new User { Name = "Tom", Age = 33 };
             var user2 = new User { Name = "Alice", Age = 26 };
 
-            db.Users.AddRange(user1, user2);
-            db.SaveChanges();
+            dbContext.Users.AddRange(user1, user2);
+            dbContext.SaveChanges();
         }
 
-        using (ApplicationContext db = new(connString))
+        using (var dbContext = CreateDBContext())
         {
-            var users = db.Users.ToList();
+            var users = dbContext.Users.ToList();
             Console.WriteLine("Users list:");
             foreach (User u in users)
             {
                 Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
             }
         }
+    }
+
+    private static ApplicationContext CreateDBContext()
+    {
+        var connString = configuration.GetConnectionString("Default");
+        if (connString is null)
+        {
+            throw new NullReferenceException("Default connection string is empty");
+        }
+
+        return new(connString);
     }
 
     private static IConfiguration GetAppSettingsFile()
